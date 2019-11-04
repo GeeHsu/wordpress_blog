0d8b622fb4a21ba44ec34490705507444bb4e5ee
geehsu <james81429@yahoo.com.tw>
2019-11-04 18:27:45 +0800

    update

--- wp-content/themes/scratchpad/footer.php
+++ wp-content/themes/scratchpad/footer.php
@@ -43,15 +43,6 @@
 				<?php printf( esc_html__( 'Blog: %1$s by %2$s.', 'watergrass' ), 'Watergrass', '<a href="https://www.facebook.com/watergrass0402/" rel="author" target="_blank">Gee Hsu</a>' ); ?>
 			</div>
 
-			<div class="site-info">
-				<!-- <?php get_template_part( 'images/inline', 'line.svg' ); ?> -->
-				<?php scratchpad_social_menu(); ?>
-
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'scratchpad' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'scratchpad' ), 'WordPress' ); ?></a>
-				<span class="sep"> | </span>
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'scratchpad' ), 'Scratchpad', '<a href="http://wordpress.com/themes/" rel="designer">Automattic</a>' ); ?>
-			</div><!-- .site-info -->
-
 		</div><!-- .wrap -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->

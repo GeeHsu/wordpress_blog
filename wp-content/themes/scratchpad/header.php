dcbfd9ceb8fedf2f5ecfdecf72d365dcf7b79de2
geehsu <james81429@yahoo.com.tw>
2019-11-05 10:39:18 +0800

    update header title

--- wp-content/themes/scratchpad/header.php
+++ wp-content/themes/scratchpad/header.php
@@ -12,6 +12,8 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+
+<title>Wordpress_blog</title>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
